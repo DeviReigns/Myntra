cbd1eac71a9a2052439913bde068a9e9ba58cf4c
Devidas <mayurgawade150598@gmail.com>
2023-02-22 23:07:52 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from "react-router-dom";
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+    <Provider store={store}>
+    <App/>
+    </Provider>
+    
+  </BrowserRouter>
 
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+reportWebVitals();
